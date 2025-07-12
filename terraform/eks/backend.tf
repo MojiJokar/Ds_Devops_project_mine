14acafad1b528ff38493d57718adee67c85da09f
ds_devops_project <mmb2020max@gmail.com>
2025-07-12 14:20:20 +0200

    eks folder upgraded

--- terraform/eks/backend.tf
+++ terraform/eks/backend.tf
@@ -1,9 +1,10 @@
-
 terraform {
   backend "s3" {
     bucket = "dev-dev-frontend-assets"
     key    = "eks/terraform.tfstate"
     region = "eu-west-3"
-    
+    # Optionally add:
+    # encrypt = true
+    # dynamodb_table = "terraform_locks"
   }
 }
--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,79 +1,6 @@
-/*variable "cluster_name" {}
-variable "cluster_version" {}
-variable "vpc_id" {}
-variable "subnet_ids" {
-  type = list(string)
-}
-variable "tags" {
-  type = map(string)
-}
-variable "eks_managed_node_groups" {
-  type = any
-}
-variable "cluster_endpoint_public_access" {
-  type    = bool
-  default = true
-}
-
-output "cluster_id" {
-  value = module.eks.cluster_id
-}
-
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  value = module.eks.cluster_security_group_id
-}
-
-output "kubeconfig" {
-  value = module.eks.kubeconfig
-}
-
-output "node_group_role_arn" {
-  value = module.eks.node_group_role_arn
-}*/
-
-/*provider "aws" {
-  region                   = var.aws_region
-  shared_credentials_files = ["/mnt/c/Users/mmb20/.aws/credentials"]
-  default_tags {
-    tags = var.default_tags
-  }
-}*/
-
-/*module "networking" {
-  source = "../modules/networking"
-
-  vpc_cidr             = var.vpc_cidr
-  environment          = var.environment
-  availability_zones   = var.availability_zones
-  public_subnet_cidrs  = var.public_subnet_cidrs
-  private_subnet_cidrs = var.private_subnet_cidrs
-}*/
-
-
-/*module "security_group" {
-  source              = "../modules/security-groups"
-  ec2_sg_name         = "SG for EC2 to enable SSH(22), HTTPS(443) and HTTP(80)"
-  vpc_id              = module.networking.vpc_id
-  ec2_jenkins_sg_name = "Allow port 8080 for jenkins"
-}
-
-module "jenkins" {
-  source                    = "../modules/jenkins"
-  ami_id                    = var.ec2_ami_id 
-  instance_type             = "t2.medium"
-  tag_name                  = "Jenkins:Ubuntu Linux EC2"
-  public_key                = var.public_key
-  subnet_id                 = tolist(module.networking.public_subnet)[0]
-  sg_for_jenkins            = [module.security_group.sg_ec2_sg_ssh_http_id, module.security_group.sg_ec2_jenkins_port_8080]
-  enable_public_ip_address  = true
-  user_data_install_jenkins = file("../modules/jenkins-runner-script/jenkins-install.sh")
-
-}*/
 terraform {
+  required_version = ">= 1.3.2"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -82,18 +9,19 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "us-east-1" # <-- CHANGE to your preferred AWS region
+}
 
-
-
-
+# VPC Module
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
 
   name = "jenkins-vpc"
   cidr = var.vpc_cidr
 
-  azs = data.aws_availability_zones.azs.names
-
+  azs             = data.aws_availability_zones.azs.names
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
@@ -114,14 +42,15 @@ module "vpc" {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
     "kubernetes.io/role/internal-elb"      = 1
   }
-
 }
 
+# EKS Module
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
   cluster_name    = "my-eks-cluster"
-  cluster_version = "1.24"
+  cluster_version = "1.24" # <-- CHANGE if you want a newer version
 
   cluster_endpoint_public_access = true
 
@@ -143,3 +72,18 @@ module "eks" {
     Terraform   = "true"
   }
 }
+
+# Data source for availability zones
+data "aws_availability_zones" "azs" {}
+
+# Variables (define these in variables.tf or here for quick testing)
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+variable "private_subnets" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+variable "public_subnets" {
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
